16c743ba2b1f91de533dbeede4fcb19f70062027
ljw1004 <lu@wischik.com>
2015-07-24 11:35:19 -0700

    Let's make the GameOfLife start on "CPU" rather than "GPU"...

--- Win2d/GameOfLife/App.xaml.vb
+++ Win2d/GameOfLife/App.xaml.vb
@@ -12,7 +12,7 @@ NotInheritable Class App
 
         Dim rootFrame As Frame = TryCast(Window.Current.Content, Frame)
         If rootFrame Is Nothing Then rootFrame = New Frame() : Window.Current.Content = rootFrame
-        If rootFrame.Content Is Nothing Then rootFrame.Navigate(GetType(MainPageGPU), e.Arguments)
+        If rootFrame.Content Is Nothing Then rootFrame.Navigate(GetType(MainPageCPU), e.Arguments)
         Window.Current.Activate()
     End Sub
 
