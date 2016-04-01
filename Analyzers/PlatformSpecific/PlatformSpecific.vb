6e7740016a7c193f58c6d210a51c596134074d42
ljw1004 <lu@wischik.com>
2016-04-01 13:47:49 -0700

    Removed PlatformSpecificAnalyzer to go into its own repo

--- Analyzers/PlatformSpecific/PlatformSpecific.vb
+++
@@ -1,6 +0,0 @@
-﻿Namespace Global.System.Runtime.CompilerServices
-    <AttributeUsage(AttributeTargets.Constructor Or AttributeTargets.Method Or
-                    AttributeTargets.Field Or AttributeTargets.Property)>
-    Public NotInheritable Class PlatformSpecificAttribute : Inherits Attribute
-    End Class
-End Namespace
