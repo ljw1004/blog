1d99d95183920bde7bf7329d9f89195f56a709f5
Lucian Wischik <lu@wischik.com>
2017-04-12 16:40:09 +0100

    Added a "distribution" approach, similar to the computation that happens in OCaml's HANSEI library

---
+++ Async/Probabilistic/Distributions.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+static class Distributions {
+
+  static int count = 0;
+
+  public static void Test() {
+    Stopwatch s;
+    var observed = new[] { 50, 50, 49, 48, 47, 48, 46 };
+
+    for (int repeat = 0; repeat < 3; repeat++) {
+      Console.WriteLine("*** EAGER PRUNE");
+      s = Stopwatch.StartNew();
+      count = 0;
+      foreach (var (p, state) in Normalize(FinalStateOfReadingsThatMatchFast(observed))) {
+        Console.WriteLine($"p({state}) = {p:.000}");
+      }
+      Console.WriteLine($"{count} traces in {s.Elapsed.TotalSeconds:.00} seconds");
+
+      Console.WriteLine("*** NAIVE FULL-STATE EXPLORATION");
+      s = Stopwatch.StartNew();
+      count = 0;
+      foreach (var (p, state) in Normalize(FinalStateOfReadingsThatMatch(observed))) {
+        Console.WriteLine($"p({state}) = {p:.000}");
+      }
+      Console.WriteLine($"{count} traces in {s.Elapsed.TotalSeconds:.0} seconds");
+    }
+  }
+
+  struct State {
+    public readonly int Location;
+    public readonly int Target;
+    
+    public bool IsAtPlace => Target == -1;
+    public bool IsInTransit => Target != -1;
+    public static State AtPlace(int loc) { return new State(loc, -1); }
+    public static State InTransit(int loc, int target) { return new State(loc, target); }
+    private State(int loc, int target) { Location = loc; Target = target; }
+    public override string ToString() => IsAtPlace ? $"At {Location}" : $"InTransit {Location}->{Target}";
+  }
+
+  static IEnumerable<(double,State)> NextState(State s) {
+    if (s.IsAtPlace) {
+      foreach (var (p, shouldStay) in Flip(0.8)) {
+        if (shouldStay) {
+          yield return (p, s);
+        } else {
+          var targets = (s.Location == 0) ? new[] { 20, 50 } : (s.Location == 20) ? new[] { 0, 50 } : new[] { 0, 20 };
+          foreach (var (q, newDest) in Uniform(targets)) {
+            yield return (p * q, State.InTransit(s.Location, newDest));
+          }
+        }
+      }
+    } else {
+      if (s.Location == s.Target) {
+        yield return (1.0, State.AtPlace(s.Location));
+      } else if (s.Location < s.Target) {
+        yield return (1.0, State.InTransit(s.Location + 1, s.Target));
+      } else {
+        yield return (1.0, State.InTransit(s.Location - 1, s.Target));
+      }
+    }
+  }
+
+  static IEnumerable<(double, State)> GenerateInitialStates() {
+    foreach (var (p, loc) in Uniform(new[] { 0, 20, 50 })) {
+      foreach (var (q, flip) in Flip(0.5)) {
+        if (flip) {
+          yield return (p * q, State.AtPlace(loc));
+        } else {
+          foreach (var (r, start) in UniformRange(0, 60)) {
+            yield return (p * (1-q) * r, State.InTransit(start, loc));
+          }
+        }
+      }
+    }
+  }
+
+  static IEnumerable<(double, (IEnumerable<int>, State))> GenerateReadingsAndSubsequentState(int num, State initialState) {
+    if (num == 0) {
+      yield return (1, (new int[] {}, initialState));
+    } else { 
+      foreach (var (p, nextState) in NextState(initialState)) {
+        foreach (var (q, sensor) in SensorReading(nextState)) {
+          foreach (var (r, (readings,subsequentState)) in GenerateReadingsAndSubsequentState(num - 1, nextState)) {
+            yield return (p * q * r, (new[] { sensor }.Concat(readings), subsequentState));
+          }
+        }
+      }
+    }
+  }
+
+  static IEnumerable<(double, (IEnumerable<int>, State))> GenerateReadingsThatMatchAndSubsequentState(int i, IList<int> observed, State initialState) {
+    if (i == observed.Count) {
+      yield return (1, (new int[] { }, initialState));
+    } else {
+      foreach (var (p, nextState) in NextState(initialState)) {
+        foreach (var (q, sensor) in SensorReading(nextState)) {
+          if (sensor == observed[i]) {
+            foreach (var (r, (readings, subsequentState)) in GenerateReadingsThatMatchAndSubsequentState(i+1, observed, nextState)) {
+              yield return (p * q * r, (new[] { sensor }.Concat(readings), subsequentState));
+            }
+          }
+        }
+      }
+    }
+  }
+
+
+  static IEnumerable<(double, State)> FinalStateOfReadingsThatMatch(IList<int> observed) {
+    foreach (var (p, initialState) in GenerateInitialStates()) {
+      foreach (var (q, (readings, subsequentState)) in GenerateReadingsAndSubsequentState(observed.Count, initialState)) {
+        count++;
+        if (readings.SequenceEqual(observed)) yield return (p * q, subsequentState);
+      }
+    }
+  }
+
+  static IEnumerable<(double, State)> FinalStateOfReadingsThatMatchFast(IList<int> observed) {
+    foreach (var (p, initialState) in GenerateInitialStates()) {
+      foreach (var (q, (readings, subsequentState)) in GenerateReadingsThatMatchAndSubsequentState(0, observed, initialState)) {
+        count++;
+        yield return (p * q, subsequentState);
+      }
+    }
+  }
+
+  static IEnumerable<(double, int)> SensorReading(State s) {
+    return new[] { (0.1, s.Location - 2), (0.2, s.Location - 1), (0.4, s.Location), (0.2, s.Location + 1), (0.1, s.Location) };
+  }
+
+  static IEnumerable<(double,T)> Uniform<T>(IList<T> values) {
+    return values.Select(v => (1.0/values.Count, v));
+  }
+
+  static IEnumerable<(double, int)> UniformRange(int min, int count) {
+    for (int i=min; i<min+count; i++) {
+      yield return (1.0 / count, i);
+    }
+  }
+
+  static IEnumerable<(double, bool)> Flip(double p) {
+    return new[] { (p, true), (1.0 - p, false) };
+  }
+
+  static IEnumerable<(double, T)> Normalize<T>(IEnumerable<(double prob, T val)> src) {
+    var grouped = src.GroupBy(pv => pv.val).Select(group => (prob: group.Select(pv => pv.prob).Sum(), val: group.Key)).ToList();
+    var sum = grouped.Select(pv => pv.prob).Sum();
+    return grouped.Select(pv => (pv.prob / sum, pv.val));
+  }
+
+}
---
+++ Async/Probabilistic/Sampling.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.ExceptionServices;
+using System.Security;
+using System.Threading;
+using System.Threading.Tasks;
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+
+
+class Sampling
+{
+    static Random RND = new Random();
+
+    public static async Task MainAsync()
+    {
+        // A probabilistic function returns a pull-based stream of values, like IEnumerable:
+        ISampleable<int> p = Foo();
+        foreach (var sample in p.Take(3)) Console.WriteLine($"sample: {sample}");
+
+        // If the probabilistic function awaits, then it returns a pull-based asynchronous stream of values, like IAsyncEnumerable.
+        IAsyncSampleable<int> q = Bar();
+        await q.Take(3).Do(sample => Console.WriteLine(sample)).ToArray();
+    }
+
+    static async ISampleable<int> Foo()
+    {
+        Console.WriteLine("Foo");
+        return RND.Next();
+    }
+
+    static async IAsyncSampleable<int> Bar()
+    {
+        Console.WriteLine("Bar");
+        await Task.Delay(500);
+        return RND.Next();
+    }
+
+}
+
+[AsyncMethodBuilder(typeof(SampleableBuilder<>))]
+public interface ISampleable<T> : IEnumerable<T> { }
+
+[AsyncMethodBuilder(typeof(AsyncSampleableBuilder<>))]
+public interface IAsyncSampleable<T> : IAsyncEnumerable<T> { }
+
+
+public class SampleableBuilder<T>
+{
+    // used by the factory
+    Sampleable _task;
+    public static SampleableBuilder<T> Create() => new SampleableBuilder<T>();
+    public void Start<TStateMachine>(ref TStateMachine stateMachine) where TStateMachine : IAsyncStateMachine => _task = new Sampleable(stateMachine);
+    public ISampleable<T> Task => _task;
+
+    // used per instance:
+    private Sampler _Sampler = null;
+    public void SetStateMachine(IAsyncStateMachine stateMachine) => throw new InvalidOperationException("SampleableBuilder<T>.SetStateMachine");
+    public void AwaitOnCompleted<TAwaiter, TStateMachine>(ref TAwaiter awaiter, ref TStateMachine stateMachine) where TAwaiter : INotifyCompletion where TStateMachine : IAsyncStateMachine => throw new InvalidOperationException("SampleableBuilder<T>.AwaitOnCompleted");
+    [SecurityCritical] public void AwaitUnsafeOnCompleted<TAwaiter, TStateMachine>(ref TAwaiter awaiter, ref TStateMachine stateMachine) where TAwaiter : ICriticalNotifyCompletion where TStateMachine : IAsyncStateMachine => throw new InvalidOperationException("SampleableBuilder<T>.AwaitUnsafeOnCompleted)");
+    public void SetException(Exception exception) => _Sampler._currentException = ExceptionDispatchInfo.Capture(exception);
+    public void SetResult(T result) => _Sampler._currentResult = result;
+
+    class Sampleable : ISampleable<T>
+    {
+        private Func<Sampler, IAsyncStateMachine> clone;
+        public IEnumerator<T> GetEnumerator() => new Sampler { clone = clone };
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        public Sampleable(IAsyncStateMachine sm)
+        {
+            clone = (Sampler) =>
+            {
+                var memberwiseCloneMethod = sm.GetType().GetMethod("MemberwiseClone", BindingFlags.Instance | BindingFlags.NonPublic);
+                var builderField = sm.GetType().GetField("<>t__builder", BindingFlags.Instance | BindingFlags.Public);
+                var clone = (IAsyncStateMachine)memberwiseCloneMethod.Invoke(sm, null);
+                builderField.SetValue(clone, new SampleableBuilder<T> { _Sampler = Sampler });
+                return clone;
+            };
+        }
+    }
+
+    class Sampler : IEnumerator<T>
+    {
+        public Func<Sampler, IAsyncStateMachine> clone;
+        public T _currentResult;
+        public ExceptionDispatchInfo _currentException;
+
+        public T Current
+        {
+            get
+            {
+                if (_currentException != null) _currentException.Throw();
+                return _currentResult;
+            }
+        }
+
+        public bool MoveNext()
+        {
+            _currentException = null;
+            clone(this).MoveNext();
+            return true;
+        }
+
+        object IEnumerator.Current => Current;
+        public void Dispose() { }
+        public void Reset() { }
+    }
+}
+
+public class AsyncSampleableBuilder<T>
+{
+    // used by the factory
+    Sampleable _task;
+    public static AsyncSampleableBuilder<T> Create() => new AsyncSampleableBuilder<T>();
+    public void Start<TStateMachine>(ref TStateMachine stateMachine) where TStateMachine : IAsyncStateMachine => _task = new Sampleable(stateMachine);
+    public IAsyncSampleable<T> Task => _task;
+
+    // used per instance:
+    private Sampler _Sampler = null;
+    public void SetStateMachine(IAsyncStateMachine stateMachine) => throw new InvalidOperationException("SampleableBuilder<T>.SetStateMachine");
+    public void AwaitOnCompleted<TAwaiter, TStateMachine>(ref TAwaiter awaiter, ref TStateMachine stateMachine) where TAwaiter : INotifyCompletion where TStateMachine : IAsyncStateMachine => awaiter.OnCompleted(_Sampler._currentSm.MoveNext);
+    [SecurityCritical] public void AwaitUnsafeOnCompleted<TAwaiter, TStateMachine>(ref TAwaiter awaiter, ref TStateMachine stateMachine) where TAwaiter : ICriticalNotifyCompletion where TStateMachine : IAsyncStateMachine => awaiter.UnsafeOnCompleted(_Sampler._currentSm.MoveNext);
+    public void SetException(Exception exception) => _Sampler._current.SetException(exception);
+    public void SetResult(T result) => _Sampler._current.SetResult(result);
+
+    class Sampleable : IAsyncSampleable<T>
+    {
+        private Func<Sampler, IAsyncStateMachine> clone;
+        public IAsyncEnumerator<T> GetEnumerator() => new Sampler { clone = clone };
+
+        public Sampleable(IAsyncStateMachine sm)
+        {
+            clone = (Sampler) =>
+            {
+                var memberwiseCloneMethod = sm.GetType().GetMethod("MemberwiseClone", BindingFlags.Instance | BindingFlags.NonPublic);
+                var builderField = sm.GetType().GetField("<>t__builder", BindingFlags.Instance | BindingFlags.Public);
+                var clone = (IAsyncStateMachine)memberwiseCloneMethod.Invoke(sm, null);
+                builderField.SetValue(clone, new AsyncSampleableBuilder<T> { _Sampler = Sampler });
+                return clone;
+            };
+        }
+
+    }
+
+    class Sampler : IAsyncEnumerator<T>
+    {
+        public Func<Sampler, IAsyncStateMachine> clone;
+        public IAsyncStateMachine _currentSm;
+        public TaskCompletionSource<T> _current;
+
+        public T Current
+        {
+            get
+            {
+                var awaiter = _current.Task.GetAwaiter();
+                if (awaiter.IsCompleted) return awaiter.GetResult();
+                throw new InvalidOperationException("IAsyncSampleableMT< - must await MoveNext() before obtaining Current");
+            }
+        }
+
+        public Task<bool> MoveNext(CancellationToken cancel)
+        {
+            _current = new TaskCompletionSource<T>();
+            _currentSm = clone(this);
+            _currentSm.MoveNext();
+            var tcs = new TaskCompletionSource<bool>();
+            _current.Task.ContinueWith(_ =>
+            {
+                tcs.SetResult(true);
+            });
+            return tcs.Task;
+        }
+
+        public void Dispose() { }
+    }
+}
+
